32de7a48c12fe171e579f5ff34f68507244afd3f
Elizabeth Hudnott <lizzy.hudnott@gmail.com>
2021-04-23 13:53:39 +0100

    Start clarifying API.

--- js/canvas-lib.js
+++ js/canvas-lib.js
@@ -35,12 +35,23 @@ export class Drawing {
 		}
 	}
 
+	scaleRectangle(x, y, width, height) {
+		const scaleX = this.scaleX;
+		const scaleY = this.scaleY;
+		const scaledX = x * scaleX
+		const roundedX = Math.round(scaledX);
+		const roundedWidth = Math.round(width * scaleX + roundedX - scaledX);
+		const scaledY = y * scaleY;
+		const roundedY = Math.round(scaledY);
+		const roundedHeight = Math.round(height * scaleY + roundedY - scaledY);
+		return [roundedX, roundedY, roundedWidth, roundedHeight];
+	}
+
 	closePath() {
 		this.context.closePath();
 	}
 
-	fill(color) {
-		this.context.fillStyle = color.toValue();
+	fill() {
 		this.context.fill();
 	}
 
@@ -67,6 +78,21 @@ export class Drawing {
 		context.moveTo(Math.round(x * this.scaleX), Math.round(y * this.scaleY));
 	}
 
+	clearRect(x, y, width, height) {
+		this.nudge(false);
+		this.context.clearRect(...this.scaleRectangle(x, y, width, height));
+	}
+
+	fillRect(x, y, width, height) {
+		this.nudge(false);
+		this.context.fillRect(...this.scaleRectangle(x, y, width, height));
+	}
+
+	strokeRect(x, y, width, height) {
+		this.nudge(true);
+		this.context.strokeRect(...this.scaleRectangle(x, y, width, height));
+	}
+
 	rotate(deltaRotation) {
 		this.context.rotate(deltaRotation * 2 * Math.PI);
 	}
@@ -76,13 +102,15 @@ export class Drawing {
 		this.scaleY *= scaleYMultiple;
 	}
 
-	setScale(scaleX, scaleY = scaleX) {
-		this.scaleX = scaleX;
-		this.scaleY = scaleY;
+	setFill(style) {
+		this.context.fillStyle = style.toValue();
 	}
 
-	stroke(color) {
-		this.context.strokeStyle = color.toValue();
+	setStroke(style) {
+		this.context.strokeStyle = style.toValue();
+	}
+
+	stroke() {
 		this.context.stroke();
 	}
 
@@ -98,20 +126,71 @@ export class Color {
 		this.saturation = saturation;
 		this.lightness = lightness;
 		this.opacity = opacity;
-		this.str = hsla(hue, saturation, lightness, opacity);
+		this.str = undefined;
 	}
 
-	hueShift(amount) {
-		let hue = this.hue + amount;
-		return new Color(hue, this.saturation, this.lightness, this.opacity);
+	clone() {
+		return new Color(this.hue, this.saturation, this.lightness, this.opacity);
+	}
+
+	setHue(amount) {
+		this.hue = amount;
+		this.str = undefined;
+	}
+
+	setSaturation(amount) {
+		this.saturation = amount;
+		this.str = undefined;
+	}
+
+	setLightness(amount) {
+		this.lightness = amount;
+		this.str = undefined;
+	}
+
+	setOpacity(amount) {
+		this.opacity = amount;
+		this.str = undefined;
 	}
 
 	toValue() {
+		if (this.str === undefined) {
+			this.str = hsla(hue, saturation, lightness, opacity);
+		}
 		return this.str;
 	}
 
 }
 
 export const Colors = {
+	RED: new Color(0),
+	ORANGE: new Color(30),
+	AMBER: new Color(45),
+	YELLOW: new Color(60),
+	LIME: new Color(75),
+	LAWN: new Color(120),
+	CYAN: new Color(180),
+	AZURE: new Color(210),
+	BLUE: new Color(240),
+	VIOLET: new Color(270),
+	MAGENTA: new Color(300),
+	CRIMSON: new Color(348, 0.83, 0.47),
+
+	MAROON: new Color(0, 1, 0.25),			// Darkened RED
+	BROWN: new Color(30, 1, 0.25),			// Darkened ORANGE
+	OLIVE: new Color(60, 1, 0.25),			// Darkened YELLOW
+	GREEN: new Color(120, 1, 0.25),			// Darkened LAWN
+	TEAL: new Color(180, 1, 0.25),			// Darkened CYAN
+	NAVY: new Color(240, 1, 0.25),			// Darkened BLUE
+	INDIGO: new Color(270, 1, 0.25),		// Darkened VIOLET
+	PURPLE: new Color(300, 1, 0.25),		// Darkened MAGENTA
+
+	CORN: new Color(54, 0.95, 0.68),
+	MINT: new Color(120, 1, 0.8),
+	BABY_BLUE: new Color(199, 0.77, 0.74),
+	PINK: new Color(330, 1, 0.8),
+
 	BLACK: new Color(0, 0, 0),
+	GRAY: new Color(0, 0, 0.63),
+	WHITE: new Color(0, 0, 1),
 };
--- js/ui.js
+++ js/ui.js
@@ -6,24 +6,7 @@ const canvas = document.getElementById('canvas');
 window.drawing = new Drawing();
 drawing.setContext(canvas.getContext('2d'));
 
-function square(color) {
-	drawing.newShape(0, 0);
-	drawing.lineTo(1, 0);
-	drawing.lineTo(1, 1);
-	drawing.lineTo(0, 1);
-	drawing.closePath();
-	drawing.fill(color);
-}
-
 window.drawPicture = function () {
-	drawing.moveAbsolute(100, 100, 100);
-	const color = new Color(0);
-	square(color);
-
-	drawing.moveRelative(1.1, 0.25, 0);
-	drawing.setScale(50);
-	const color2 = color.hueShift(30);
-	square(color2);
 }
 
 let workspace;
@@ -68,6 +51,7 @@ fetch('blocks.json')
 			startScale: 0.9,
 		}
 	});
+	workspace.toolbox_.flyout_.autoClose = false;
 
 	resize();
 
